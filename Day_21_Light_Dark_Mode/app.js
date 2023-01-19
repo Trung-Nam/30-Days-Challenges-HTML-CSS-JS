77b7d95fdd8ab3e37507c1f8ea9459a0b8c5a4ae
Trung Nam <trungnam.work@gmail.com>
2023-01-19 22:56:20 +0700

    Day 21 Light Dark Mode

---
+++ Day_21_Light_Dark_Mode/app.js
@@ -0,0 +1,12 @@
+var swBtn = document.querySelector('label');
+var body = document.querySelector('body');
+function init() {
+    let mode =localStorage.getItem('mode');
+    body.setAttribute('class', mode);
+}
+init();
+swBtn.addEventListener('click',()=>{
+    body.classList.toggle('dark');
+    let mode= body.getAttribute('class') ? "dark" : "";
+    localStorage.setItem('mode',mode);
+});
